36cb7da6b382e2c354cffec7f23fc1831fd74e42
oshkaStudy <oshkax2@gmail.com>
2025-05-12 21:22:38 +0300

    changed txt

--- src/main/java/oshka_guru_qa/Main.java
+++ src/main/java/oshka_guru_qa/Main.java
@@ -2,6 +2,6 @@ package oshka_guru_qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, epta!");
     }
 }
